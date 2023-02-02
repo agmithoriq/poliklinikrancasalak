a91fbcabdec41791eacb739b315d8e878ade1bce
unknown <agmithoriq@gmail.com>
2023-02-02 13:20:08 +0700

    first commit

---
+++ home.php
@@ -0,0 +1,15 @@
+<b>
+    <font color="#00b994">DASHBOARD</font>
+</b>
+<br><br>
+<hr color="#00b994" size="2px" width="100%">
+<br>
+<font size="+3"><b>TUGAS BESAR UAS KELOMPOK PWEB UAS</b></font><br><br>
+<font color="#646464" size="+1">
+    <p><b>Poliklinik Rancasalak</b> merupakan aplikasi Poliklinik berbasis web.aplikasi ini untuk memenuhi tugas besar uas Pweb</p>
+    <p><b></b> Nama Kelompok : 
+    Muhamad Dikha Priangga, 
+    Rizky Agmithoriq, 
+    Raden Azriel M.Y</p>
+</font>
+<br>
